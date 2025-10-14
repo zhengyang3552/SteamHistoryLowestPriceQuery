5e5c1933fe7134bc6bd66b66c99bdf3f21a8d9de
正阳 <lizhengyang27@outlook.com>
2025-10-14 23:36:18 +0800

    Add files via upload

--- SteamHistoryLowestPriceQuery.js
+++ SteamHistoryLowestPriceQuery.js
@@ -11,7 +11,7 @@
 // @updateURL https://raw.githubusercontent.com/zhengyang3552/SteamHistoryLowestPriceQuery/main/SteamHistoryLowestPriceQuery.js
 // @author      正阳
 // @license     GPL version 3 or any later version
-// @version     1.7
+// @version     1.6
 // @grant       GM_xmlhttpRequest
 // @enable      true
 // jshint esversion:6
